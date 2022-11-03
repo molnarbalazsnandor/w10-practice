d7ff46e8d841fc77f3244170f7cf5776ab26bcb5
molnarbalazsnandor <molnarbalazsnandor@gmail.com>
2022-11-03 09:33:27 +0100

    11-02

---
+++ frontend/script.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("#add-category");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // megtiltja a html elem alapműködését (itt pl nem megyünk majd át a máv oldalra)
+  const product = {
+    name: document.querySelector("#name").value,
+    category: document.querySelector("#category").value,
+    price: parseInt(document.querySelector("#price").value),
+  };
+  fetch("/product", {
+    method: "POST",
+    body: JSON.stringify(product),
+  });
+  //megmondjuk a szervernek, hogy ez egy post request(ezt most nem tudom, mi), hogy mi az adat, és hogy json-ban küldjük
+  // post-tal küldünk legtöbbször adatokat a backendnek; ehhez először összegyűjtjük a kívánt formátumba (pl. fenti product objektum)
+});
