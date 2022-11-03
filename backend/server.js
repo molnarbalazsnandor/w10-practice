0ebb31201bb6e3755cee2beb5e1a934428d11682
molnarbalazsnandor <molnarbalazsnandor@gmail.com>
2022-11-03 14:00:43 +0100

    ghg

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
+const { response } = require("express");
+const fileUpload = require("express-fileupload");
 const express = require("express");
 const app = express();
 const port = 3000;
 
 const path = require("path");
 //az elérési útvonal
+//require: importálás
+
+const fs = require("fs");
+//fs: filesystem
+
+app.use(express.json());
+//innen tudja a backend, hogy json-ban kapja a frontend által küldött infót: megjelenik a terminalban
+
+app.use(fileUpload());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
@@ -28,6 +39,43 @@ De az index.html-ben javítani kell az útvonalakat!
 
 app.use("/public", express.static(`${__dirname}/../frontend/public`));
 
+app.post("/upload", (request, response) => {
+  console.log(request.body);
+  fs.writeFileSync(
+    `${__dirname}/data/userdata.json`,
+    JSON.stringify(request.body, null, 4),
+    (error) => {
+      if (error) {
+        console.log(error);
+        return response.status(500).send(error);
+      } else {
+        response.status(200).send("ok");
+      }
+    }
+  );
+});
+
+app.post("/upload-image", (request, response) => {
+  if (!request.files) {
+    return response.status(400).send("no files were uploaded");
+  }
+  const picture = request.files.file;
+  const picName = request.body.fileName;
+  console.log(picName);
+
+  picture.mv(`${__dirname}/data/${picName}.jpg`, (error) => {
+    if (error) {
+      console.log(error);
+      return response.status(500).send(error);
+    } else {
+      response.status(200).send("image ok");
+    }
+  });
+  //mv: move: átmozgatja másik mappába
+});
+
+//az első mindig a request, a második a response(de bármilyen neve lehet a változóknak)
+//kell küldeni egy response-t különben sokáig ötltene az oldal (a 200-as státusz az hogy betöltött)
 app.listen(port, () => {
   console.log(`Server is running at: http://127.0.0.1:${port}`);
 });
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -7,10 +7,29 @@ form.addEventListener("submit", function (event) {
     category: document.querySelector("#category").value,
     price: parseInt(document.querySelector("#price").value),
   };
-  fetch("/product", {
+
+  const formData = new FormData();
+
+  formData.append("fileName", document.querySelector("#fileName").value);
+
+  formData.append("file", document.querySelector("#file").files[0]);
+  //ez tömböt ad vissza, egy elem esetén is, ezért kell a [0]
+  //append: feltölti adattal a formData-t
+
+  fetch("/upload", {
     method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
     body: JSON.stringify(product),
   });
   //megmondjuk a szervernek, hogy ez egy post request(ezt most nem tudom, mi), hogy mi az adat, és hogy json-ban küldjük
   // post-tal küldünk legtöbbször adatokat a backendnek; ehhez először összegyűjtjük a kívánt formátumba (pl. fenti product objektum)
+
+  // adatot json ban, képet formDatával küldjünk
+
+  fetch("/upload-image", {
+    method: "POST",
+    body: formData,
+  });
 });
